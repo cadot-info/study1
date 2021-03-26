d3e0bdf0bee03233541eaa7c28723b437eec4789
michael <michael@cadot.info>
2021-03-26 17:19:43 +0100

    env:add boottswatch, configure sass

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,15 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
-import './bootstrap';
+//import './bootstrap';
+const $ = require('jquery');
+
+
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('animate.css')

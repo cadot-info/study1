2f6052040955ed11d9256670c56b5ec771171ba6
michael <michael@cadot.info>
2021-03-28 10:07:32 +0200

    fix:admin after login

--- src/Security/AppAuthentificatorAuthenticator.php
+++ src/Security/AppAuthentificatorAuthenticator.php
@@ -96,7 +96,7 @@ class AppAuthentificatorAuthenticator extends AbstractFormLoginAuthenticator imp
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
         throw new \Exception('TODO: provide a valid redirect inside ' . __FILE__);
     }
 
